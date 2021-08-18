c152e1d4ed18a1931ede20426a32b2b6c164323e
Utshab Khadka <khadkautshav7@gmail.com>
2021-08-18 11:37:50 -0400

    API Fixed

--- WebServiceAPI/Controllers/CalculatorAPIController.cs
+++ WebServiceAPI/Controllers/CalculatorAPIController.cs
@@ -1,8 +1,11 @@
 ﻿using System;
+
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using WebServiceFinalLibrary;
+
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -15,5 +18,34 @@ namespace WebServiceAPI.Controllers
         {
             return View();
         }
+        [Route("api/[controller]/[action]")]
+        [ApiController]
+        public class CalculatorController : ControllerBase
+        {
+            [HttpGet]
+            public double addition(double val1, double val2)
+            {
+                return Calculator.addition(val1, val2);
+            }
+
+            [HttpGet]
+            public double subtraction(double val1, double val2)
+            {
+                return Calculator.subtraction(val1, val2);
+            }
+
+            [HttpGet]
+            public double multipication(double val1, double val2)
+            {
+                return Calculator.multipication(val1, val2);
+            }
+
+            [HttpGet]
+            public double division(double val1, double val2)
+            {
+                return Calculator.division(val1, val2);
+            }
+        }
     }
 }
+
